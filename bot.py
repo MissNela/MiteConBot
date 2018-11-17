5356e7614bb40cd7e79e08fdc40b4069427e85a2
MissNela <44345612+MissNela@users.noreply.github.com>
2018-11-17 01:34:13 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -121,7 +121,7 @@ async def hosting():
     r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
     embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
     embed.set_author(name='Hosting')
-    embed.set_image(url = 'https://image.ibb.co/caM2BK/help.gif')
+    embed.set_image(url = 'https://cdn.discordapp.com/attachments/444585767210057728/513149361027284992/1_Bwp5z9qlQAor4hw2K61gIw.jpeg')
     embed.add_field(name = '``Support Server`` ',value ='https://discord.gg/A36parZ',inline = False)
     embed.add_field(name = 'Github',value ='https://www.github.com',inline = False)
     embed.add_field(name = 'Heroku',value ='https://heroku.com',inline = False)
